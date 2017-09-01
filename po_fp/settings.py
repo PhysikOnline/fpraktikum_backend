f7fcc30185c4f911da1de3c8d82f4ee192f3ab54
christian <chgad.games@gmail.com>
2017-09-01 16:55:56 +0200

    changed statics again again again again again

--- po_fp/settings.py
+++ po_fp/settings.py
@@ -163,7 +163,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # # See: https://docs.djangoproject.com/en/dev/ref/settings/#static-root
 # STATIC_ROOT = ''
