00d4e809743392798861cf261de6432409c52e5b
chgad <chgad.games@gmail.com>
2017-09-13 13:06:15 +0200

    added old relict

--- fpraktikum/api_urls.py
+++ fpraktikum/api_urls.py
@@ -5,7 +5,7 @@ from fpraktikum.api_views import *
 app_name = "api"
 urlpatterns = [
     url(r'^registration/', RegistrationView.as_view(), name=RegistrationView.name),
-    url(r'^user/(?P<user_snumber>.+)/', UserCheckView.as_view(), name=UserCheckView.name),
+    url(r'^user/(?P<user_login>.+)/', UserCheckView.as_view(), name=UserCheckView.name),
     url(r'^register/', SetRegistrationView.as_view(), name=SetRegistrationView.name),
     url(r'^accept_decline/', AcceptDeclinePartnershipView.as_view(), name=AcceptDeclinePartnershipView.name),
     url(r'test/', TestIlDbView.as_view()),
