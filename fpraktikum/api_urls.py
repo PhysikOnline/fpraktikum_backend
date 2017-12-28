711566e6e7e2ed40d74e2735f893e10bd28cd20b
chgad <chgad.games@gmail.com>
2017-12-28 14:59:43 +0100

    re arangement

--- fpraktikum/api_urls.py
+++ fpraktikum/api_urls.py
@@ -15,7 +15,7 @@ app_name = "api"
 urlpatterns = [
     url(r'^user/(?P<user_login>.+)/', UserCheckView.as_view(), name=UserCheckView.name),
     url(r'^check_partner/', CheckPartnerView.as_view(), name=CheckPartnerView.name),
-    #url(r'test/', TestIlDbView.as_view()),
+    # url(r'test/', TestIlDbView.as_view()),
     # url(r'^registration/', RegistrationView.as_view(), name=RegistrationView.name),
     # url(r'^register/{pk}/', SetRegistrationView.as_view(), name=SetRegistrationView.name),
     # url(r'^accept/', AcceptDeclinePartnershipView.as_view(), name=AcceptDeclinePartnershipView.name),
