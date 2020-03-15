9e094339dc3c21533f22b7f0c5866f3aae55713f
christian <chgad.games@gmail.com>
2020-03-15 13:20:24 +0100

    added bool in wsgi

--- po_fp/wsgi.py
+++ po_fp/wsgi.py
@@ -11,11 +11,11 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-if not os.environ.get("DEBUG", default=False):
+if not bool(os.environ.get("DEBUG", default=False)):
     from raven.contrib.django.raven_compat.middleware.wsgi import Sentry
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "po_fp.settings")
 
 application = get_wsgi_application()
-if not os.environ.get("DEBUG", default=False):
+if bool(os.environ.get("DEBUG", default=False)):
     application = Sentry(application)
